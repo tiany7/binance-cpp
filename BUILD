7d4e4c3b7992801f46d46735a68bb8343aafb644
tiany7 <yt2825@columbia.edu>
2024-02-06 09:54:55 -0500

    [infra] bazel can be properly included

---
+++ BUILD
@@ -0,0 +1,7 @@
+cc_library(
+    name = "cpp_binance_connector",
+    deps = ["//src:binance_lib"],
+    includes = ["binance"],
+    alwayslink = True,
+    visibility = ["//visibility:public"],
+)
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
-#include <binance.h>
+#include "src/binance/binance.h"
 
 int main()
 {
-    //Try out the API Wrapper here
+    Binance::MarketData market;
+    json result;
+    std::cout<<"Binance API C++"<<std::endl;
+    market.SetApiKeys("api-key", "secret-key");
+    std::cout<<"Binance API C++ after"<<std::endl;
+
+    // test connectivity to the Rest API
+    market.TestConnectivity(result);
+    std::cout <<"ok "<< result << std::endl;
+
+    // current average price for a symbol
+    MarketSymbolParams params;
+    params.symbol = "BNBBTC";
+    market.CurrentAvgPrice(params, result);
+    std::cout << result << std::endl;
+
     return 0;
 }
